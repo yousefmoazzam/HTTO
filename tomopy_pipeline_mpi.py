8a5fd9d4f2ad95b734842694c63bdcb5656a9bf9
Daniil Kazantsev <dkazanc@hotmail.com>
2022-08-24 18:10:46 +0100

    GPU works serially

--- tomopy_pipeline_mpi.py
+++ tomopy_pipeline_mpi.py
@@ -8,6 +8,7 @@ import chunk_h5
 from datetime import datetime
 import os
 
+import GPUtil
 
 def __option_parser():
     parser = argparse.ArgumentParser()
@@ -19,8 +20,9 @@ def __option_parser():
     parser.add_argument("-d", "--dimension", type=int, choices=[1, 2, 3], default=1,
                         help="Which dimension to slice through (usually 1 = projections, 2 = sinograms).")
     parser.add_argument("-cr", "--crop", type=int, choices=range(1, 101), default=100,
-                        help="Percentage of data to process. 10 will take the middle 10% of data in the second dimension.")
-    parser.add_argument("-m", "--methods_no", type=int, default=1, help="The number of different methods to apply to data.")
+                        help="Percentage of data to process. 10 will take the middle 10% of data in the second dimension.")    
+    parser.add_argument("-rec", "--reconstruction", default="gridrec", help="The reconstruction method to apply.")
+    parser.add_argument("-rings", "--stripe", default=None, help="The stripes removal method to apply.")
     parser.add_argument("-nc", "--ncore", type=int, default=1, help="The number of cores.")
     args = parser.parse_args()
     return args
@@ -28,17 +30,20 @@ def __option_parser():
 
 def main():
     args = __option_parser()
+    comm = MPI.COMM_WORLD
+    comm_size = comm.Get_size()
+    rank = comm.Get_rank()     
     for i in range(args.repeat):
         print(f"Run number: {i}")
         total_time0 = MPI.Wtime()
-        with h5.File(args.in_file, "r", driver="mpio", comm=MPI.COMM_WORLD) as in_file:
+        with h5.File(args.in_file, "r", driver="mpio", comm=comm) as in_file:
             dataset = in_file[args.path]
             shape = dataset.shape
         print_once(f"Dataset shape is {shape}")
-        angles_degrees = load_h5.get_angles(args.in_file, comm=MPI.COMM_WORLD)    
+        angles_degrees = load_h5.get_angles(args.in_file, comm=comm)    
         data_indices = load_h5.get_data_indices(args.in_file,
                                                 image_key_path="/entry1/tomo_entry/instrument/detector/image_key",
-                                                comm=MPI.COMM_WORLD)
+                                                comm=comm)
         angles_radians = np.deg2rad(angles_degrees[data_indices])
         
         # preview to prepeare to crop the data from the middle when --crop is used to avoid loading the whole volume
@@ -55,17 +60,17 @@ def main():
         print_once(f"Cropped data shape is {cropped_shape}")
 
         load_time0 = MPI.Wtime()
-        data = load_h5.load_data(args.in_file, args.dimension, args.path, comm=MPI.COMM_WORLD, preview=preview)
+        data = load_h5.load_data(args.in_file, args.dimension, args.path, comm=comm, preview=preview)
         load_time1 = MPI.Wtime()
         load_time = load_time1 - load_time0
         print_once(f"Raw projection data loaded in {load_time} seconds")
 
         darks, flats = load_h5.get_darks_flats(args.in_file, args.path,
                                             image_key_path="/entry1/tomo_entry/instrument/detector/image_key",
-                                            comm=MPI.COMM_WORLD, preview=preview, dim=args.dimension)
+                                            comm=comm, preview=preview, dim=args.dimension)
 
         (angles_total, detector_y, detector_x) = np.shape(data)
-        print(f"Process {MPI.COMM_WORLD.rank}'s data shape is {(angles_total, detector_y, detector_x)}")
+        print(f"Process {rank}'s data shape is {(angles_total, detector_y, detector_x)}")
         
         norm_time0 = MPI.Wtime()
         data = tomopy.normalize(data, flats, darks, ncore=args.ncore, cutoff=10)
@@ -83,11 +88,17 @@ def main():
         
         abs_out_folder = os.path.abspath(args.out_folder)
         out_folder = f"{abs_out_folder}/{datetime.now().strftime('%d-%m-%Y_%H_%M_%S')}_recon"
-        if MPI.COMM_WORLD.rank == 0:
+        if rank == 0:
             print("Making directory")
             os.mkdir(out_folder)
             print("Directory made")
 
+        nNodes = 1 # change this when nNodes > 1
+        GPUs_list = GPUtil.getAvailable() # will return a list of availble GPUs
+        GPU_index_wr_to_rank = __calculate_GPU_index(nNodes, rank, GPUs_list)
+        #print(len(GPUs_list))
+        #print(GPU_index_wr_to_rank)
+        
         # calculate the chunk size for the projection data
         slices_no_in_chunks = 4
         if (args.dimension == 1):
@@ -99,85 +110,74 @@ def main():
 
         if args.dimension == 1:
             save_time0 = MPI.Wtime()
-            chunk_h5.save_dataset(out_folder, "intermediate.h5", data, args.dimension, chunks_data, comm=MPI.COMM_WORLD)
+            chunk_h5.save_dataset(out_folder, "intermediate.h5", data, args.dimension, chunks_data, comm=comm)
             save_time1 = MPI.Wtime()
             save_time = save_time1 - save_time0
             print_once(f"Intermediate data saved in {save_time} seconds")
 
             slicing_dim = 2 # assuming sinogram slicing here to get it loaded
             reload_time0 = MPI.Wtime()
-            data = load_h5.load_data(f"{out_folder}/intermediate.h5", slicing_dim, "/data", comm=MPI.COMM_WORLD)
+            data = load_h5.load_data(f"{out_folder}/intermediate.h5", slicing_dim, "/data", comm=comm)
             reload_time1 = MPI.Wtime()
             reload_time = reload_time1 - reload_time0
             print_once(f"Data reloaded in {reload_time} seconds")
         
+
         # calculating the center of rotation 
         center_time0 = MPI.Wtime()
         rot_center = 0
-        data = np.swapaxes(data, 0, 1)
-        mid_rank = int(round(MPI.COMM_WORLD.size/2)+0.1)        
-        if MPI.COMM_WORLD.rank == mid_rank:
-            mid_slice = int(np.size(data,0)/2)
+        mid_rank = int(round(comm_size/2)+0.1)        
+        if rank == mid_rank:
+            mid_slice = int(np.size(data,1)/2)
             #print(f"Slice for calculation CoR {mid_slice}")
-            rot_center = tomopy.find_center_vo(data[mid_slice, :, :], step=0.5, ncore=args.ncore)
+            rot_center = tomopy.find_center_vo(data[:, mid_slice, :], step=0.5, ncore=args.ncore)
             #print(f"The calculated CoR is {rot_center}")
-        rot_center = MPI.COMM_WORLD.bcast(rot_center, root=mid_rank)
+        rot_center = comm.bcast(rot_center, root=mid_rank)
         center_time1 = MPI.Wtime()
         center_time = center_time1 - center_time0
         print_once(f"COR found in {center_time} seconds")
+        
+        if args.stripe is not None:
+            # removing stripes
+            stripes_time0 = MPI.Wtime()
+            if args.stripe == 'remove_stripe_fw':
+                data = tomopy.prep.stripe.remove_stripe_fw(data, wname='db5', sigma=5, ncore=args.ncore)
+            if args.stripe == 'remove_all_stripe':
+                data = tomopy.prep.stripe.remove_all_stripe(data, ncore=args.ncore)
+            if args.stripe == 'remove_stripe_based_sorting':
+                data = tomopy.prep.stripe.remove_stripe_based_sorting(data, ncore=args.ncore)
+            stripes_time1 = MPI.Wtime()
+            stripes_time = stripes_time1 - stripes_time0
+            print_once(f"Data unstriped in {stripes_time} seconds")
 
-        if (args.methods_no == 2):
-            # removing stripes
-            stripes_time0 = MPI.Wtime()
-            data = tomopy.prep.stripe.remove_stripe_fw(data, wname='db5', sigma=5, ncore=args.ncore)
-            stripes_time1 = MPI.Wtime()
-            stripes_time = stripes_time1 - stripes_time0
-            print_once(f"Data unstriped in {stripes_time} seconds")
-        if (args.methods_no == 3):
-            # removing stripes
-            stripes_time0 = MPI.Wtime()
-            data = tomopy.prep.stripe.remove_stripe_fw(data, wname='db5', sigma=5, ncore=args.ncore) 
-            stripes_time1 = MPI.Wtime()
-            stripes_time = stripes_time1 - stripes_time0
-            print_once(f"Data unstriped in {stripes_time} seconds")
-            # applying median filter
-            filter_time0 = MPI.Wtime()
-            data = tomopy.misc.corr.median_filter(data)  
-            filter_time1 = MPI.Wtime()
-            filter_time = filter_time1 - filter_time0
-            print_once(f"Data median filtered in {filter_time} seconds")
-        if (args.methods_no == 4):
-            # removing stripes
-            stripes_time0 = MPI.Wtime()
-            data = tomopy.prep.stripe.remove_all_stripe(data)    
-            stripes_time1 = MPI.Wtime()
-            stripes_time = stripes_time1 - stripes_time0
-            print_once(f"Data unstriped in {stripes_time} seconds")
-            # applying median filter
-            filter_time0 = MPI.Wtime()
-            data = tomopy.misc.corr.median_filter(data)  
-            filter_time1 = MPI.Wtime()
-            filter_time = filter_time1 - filter_time0
-            print_once(f"Data median filtered in {filter_time} seconds")
-            # applying Paganin filter
-            filter_time0 = MPI.Wtime()
-            data = tomopy.prep.phase.retrieve_phase(data)  
-            filter_time1 = MPI.Wtime()
-            filter_time = filter_time1 - filter_time0
-            print_once(f"Applying Paganin filter in {filter_time} seconds")
-            
+        # applying Paganin filter
+        #filter_time0 = MPI.Wtime()
+        #data = tomopy.prep.phase.retrieve_phase(data, ncore=args.ncore)  
+        #filter_time1 = MPI.Wtime()
+        #filter_time = filter_time1 - filter_time0
+        #print_once(f"Applying Paganin filter in {filter_time} seconds")
+
         recon_time0 = MPI.Wtime()
-        print_once(f"The actual used CoR is {rot_center}")        
-        recon = tomopy.recon(data, angles_radians, center=rot_center, algorithm='gridrec', sinogram_order=True, ncore=args.ncore)        
-        """
-        options = {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'gpu_list': [0, 1, 2, 3]}
-        recon = tomopy.recon(data,
-                     angles_radians,
-                     center=rot_center,
-                     algorithm=tomopy.astra,
-                     options=options,
-                     ncore=args.ncore)
-        """
+        print_once(f"Using CoR {rot_center}")        
+        if args.reconstruction.isupper():
+            # use ASTRA toolbox for reconstruction ona  GPU
+            if GPUs_list is not None:
+                opts = {}
+                opts['method']=args.reconstruction
+                opts['proj_type']='cuda'
+                opts['gpu_list']=[0]
+                opts['gpu_list']=[GPU_index_wr_to_rank]
+                recon = tomopy.recon(data,
+                        angles_radians,
+                        center=rot_center,
+                        algorithm=tomopy.astra,
+                        options=opts,
+                        ncore=args.ncore)
+            else:
+                raise Exception("There are no GPUs available for reconstruction")
+        else:
+            # use TomoPy methods
+            recon = tomopy.recon(np.swapaxes(data, 0, 1), angles_radians, center=rot_center, algorithm=args.reconstruction, sinogram_order=True, ncore=args.ncore)
         recon_time1 = MPI.Wtime()
         recon_time = recon_time1 - recon_time0
         print_once(f"Data reconstructed in {recon_time} seconds")
@@ -186,7 +186,7 @@ def main():
         chunks_recon = (1, recon_x, recon_y) 
         
         save_recon_time0 = MPI.Wtime()
-        chunk_h5.save_dataset(out_folder, "reconstruction.h5", recon, 1, chunks_recon, comm=MPI.COMM_WORLD)
+        chunk_h5.save_dataset(out_folder, "reconstruction.h5", recon, 1, chunks_recon, comm=comm)
         save_recon_time1 = MPI.Wtime()
         save_recon_time = save_recon_time1 - save_recon_time0
         print_once(f"Reconstruction saved in {save_recon_time} seconds")
@@ -195,10 +195,13 @@ def main():
         total_time = total_time1 - total_time0
         print_once(f"Total time = {total_time} seconds.")
 
-
 def print_once(output):
     if MPI.COMM_WORLD.rank == 0:
         print(output)
+        
+def __calculate_GPU_index(nNodes, rank, GPUs_list):
+    nGPUs = len(GPUs_list)
+    return int(rank / nNodes) % nGPUs
 
 if __name__ == '__main__':
     main()
