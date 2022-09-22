73a6e0268d182e515748873e6575647b152eb344
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-09-22 10:31:06 +0000

    Move pizdaint job script copy to run

--- pizdaint_utils/setup.sh
+++ pizdaint_utils/setup.sh
@@ -7,4 +7,3 @@ podman run \
     --publish 5000:5000 \
     --volume /scratch/$USER/registry:/var/lib/registry \
     registry:2
-scp $( dirname -- "$0"; )/container_job.sbatch Pizdaint:container_job.sbatch
