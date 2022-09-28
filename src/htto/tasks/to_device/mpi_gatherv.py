34683293ef30f86f3af1d39c5d2d4065180b43d7
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-09-28 12:58:00 +0000

    Add inplace to device

--- src/htto/tasks/to_device/mpi_gatherv.py
+++ src/htto/tasks/to_device/mpi_gatherv.py
@@ -18,7 +18,7 @@ def to_device(
             ),
             dtype=data.dtype,
         )
-        if comm.rank == 0
+        if comm.rank == device_rank
         else None
     )
     comm.Gatherv(
@@ -35,5 +35,32 @@ def to_device(
         )
         if device_data is not None
         else None,
+        device_rank,
     )
     return device_data
+
+
+def to_device_inplace(
+    data: numpy.ndarray,
+    device_data: Optional[cupy.ndarray],
+    concat_dim: int,
+    comm: Comm,
+    device_rank: int = 0,
+) -> None:
+    ranks_num_slices = comm.gather(data.shape[concat_dim])
+    comm.Gatherv(
+        data,
+        (
+            device_data,
+            [
+                prod(
+                    rank_num_slices if dim == concat_dim else dim_size
+                    for dim, dim_size in enumerate(data.shape)
+                )
+                for rank_num_slices in ranks_num_slices
+            ],
+        )
+        if device_data is not None
+        else None,
+        device_rank,
+    )
